c97eef6cda819b8368f1e9481a87a4c3499f5cbe
TSIIAM <tsi.iamesa@gmail.com>
2017-09-22 16:40:35 -0300

    first commit of misLibros

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$("document").ready(main);
+
+function main() {
+
+}
+
+
+function Book(_tittle, _img, _description, _link) {
+    this.tittle = _tittle;
+    this.img = _img;
+    this.description = _description;
+    this.link = _link;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# misLibros
+
+Repositor para tener mis libros a mano.
+
+## De que trata?
+
+Un sitio estatico donde tengo acceso a los libros que estoy leyendo y tengo para leer.
